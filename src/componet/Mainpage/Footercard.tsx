68ff1f5e84bb7f651f82680b08fde007f3f5f8e6
JenilKyada04 <jenilkyada007@gmail.com>
2026-01-02 16:04:33 +0530

    footer card

--- src/componet/Mainpage/Footercard.tsx
+++ src/componet/Mainpage/Footercard.tsx
@@ -1,9 +1,23 @@
-import React from 'react'
+import Footersmcard from "./Footersmcard";
 
 function Footercard() {
   return (
-    <div>
-      fdfdfd
+    <div className='bg-gray-200  p-10  '>
+      <div className="max-w-360 mx-auto flex justify-evenly items-center">
+
+        <div className="w-62">
+          <h1 className="font-integral font-bold text-[25px]">SHOP.CO</h1>
+          <p className="text-sm font-thin mt-6 mb-5">We have clothes that suits your style and which you’re proud to wear. From women to men.</p>
+          <div className="flex space-x-3 mt-5" >
+            <img src="/img/logo/1.png" alt="" />
+            <img src="/img/logo/2.png" alt="" />
+            <img src="/img/logo/3.png" alt="" />
+            <img src="/img/logo/4.png" alt="" />
+          </div>
+
+        </div>
+      <Footersmcard/>
+      </div>
     </div>
   )
 }
---
+++ src/componet/Mainpage/Footersmcard.tsx
@@ -0,0 +1,50 @@
+const name = [{
+    name:"Company",
+    p1:"About Us",
+    p2:"Features",
+    p3:"Works",
+    p4:"Career",
+},
+{
+   name:"Help",
+    p1:"Customer Support",
+    p2:"Delivery Details",
+    p3:"Terms & Conditions",
+    p4:"Privacy Policy",  
+},
+{
+   name:"FAQ",
+    p1:"Account",
+    p2:"Manage Deliveries",
+    p3:"Orders",
+    p4:"Payments",  
+},
+{
+   name:"Resources",
+    p1:"Free eBooks",
+    p2:"Development Tutorial",
+    p3:"How to - Blog",
+    p4:"Youtube Playlist",  
+},
+]
+
+
+function Footersmcard() {
+  return (
+    <div>
+        <div className="mt-10 grid grid-cols-4 md:grid-cols-4 gap-25">
+            {name.map((item, index) => (
+                <div key={index} className="flex flex-col gap-3">
+                    <h1 className="font-integral font-bold text-lg">{item.name}</h1>
+                    <p className="text-sm font-thin hover:underline cursor-pointer">{item.p1}</p>
+                    <p className="text-sm font-thin hover:underline cursor-pointer">{item.p2}</p>
+                    <p className="text-sm font-thin hover:underline cursor-pointer">{item.p3}</p>
+                    <p className="text-sm font-thin hover:underline cursor-pointer">{item.p4}</p>
+                </div>
+            ))}
+        </div>
+    </div>
+  )
+}
+
+export default Footersmcard
